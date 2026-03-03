9df6c931397e0c0f6e884ccf3240ac9eb493ff4e
reynaportillo445-sketch <reynaportillo445@gmail.com>
2026-03-03 15:54:43 -0600

    Primer commit ORM

---
+++ models/usuario.cs
@@ -0,0 +1,8 @@
+namespace proyectoequipo.ORM.Models;
+
+public class Usuario
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; } = string.Empty;
+    public string Correo { get; set; } = string.Empty;
+}
